cc20ed43811b2690b1f2d7dbf406b00c08e3fba0
cameronfeeman <139170459+cameronfeeman@users.noreply.github.com>
2023-09-28 21:36:26 -0400

    Printing jobs complete. The return statement for no results not finished yet

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,21 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.isEmpty()) {
+            System.out.println("Search term: " + " with No Results" );
+            System.out.println("No Results");
+        }
 
-        System.out.println("printJobs is not implemented yet");
+        for(HashMap<String,String> job : someJobs) {
+            System.out.println("***** \nposition type: " + job.get("position type"));
+            System.out.println("name: " + job.get("name"));
+            System.out.println("employer: " + job.get("employer"));
+            System.out.println("location: " + job.get("location"));
+            System.out.println("core competency: " + job.get("core competency"));
+            System.out.println("*****\n");
+
+        }
+
+
     }
 }
